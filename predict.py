8dc6c2d792f354fef1536f549b2651355c29da0f
hisuic <henryharlay@gmail.com>
2025-03-03 23:25:44 +0900

    add while loop

--- predict.py
+++ predict.py
@@ -7,18 +7,21 @@ model_dir = "checkpoint-1988"
 model = AutoModelForSequenceClassification.from_pretrained(model_dir)
 tokenizer = AutoTokenizer.from_pretrained(model_dir)
 
-# get input
-text = input("text: ")
+while True:
+    # get input
+    text = input("text(q to quit): ")
+    if text == "q":
+        break
 
-# tokenize
-inputs = tokenizer(text, return_tensors="pt")
+    # tokenize
+    inputs = tokenizer(text, return_tensors="pt")
 
-# Prediction
-outputs = model(**inputs)
+    # Prediction
+    outputs = model(**inputs)
 
-# ログを確認（分類タスクならargmaxを取る）
-logits = outputs.logits
-pred_id = logits.argmax(dim=-1).item()
+    # ログを確認（分類タスクならargmaxを取る）
+    logits = outputs.logits
+    pred_id = logits.argmax(dim=-1).item()
 
-# result
-print("Predict result: ", pred_id)
+    # result
+    print("Predict result: ", pred_id)
